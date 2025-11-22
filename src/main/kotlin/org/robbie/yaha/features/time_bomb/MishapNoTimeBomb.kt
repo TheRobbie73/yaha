735b38cbb2a663070f8278147ac2487b93ad6db7
TheRobbie72 <franc.mer2022@gamil.com>
2025-11-22 17:18:12 +0800

    book pages + explosion on no bomb mishap

--- src/main/kotlin/org/robbie/yaha/features/time_bomb/MishapNoTimeBomb.kt
+++ src/main/kotlin/org/robbie/yaha/features/time_bomb/MishapNoTimeBomb.kt
@@ -5,6 +5,7 @@ import at.petrak.hexcasting.api.casting.iota.Iota
 import at.petrak.hexcasting.api.casting.mishaps.Mishap
 import at.petrak.hexcasting.api.pigment.FrozenPigment
 import net.minecraft.util.DyeColor
+import net.minecraft.world.World
 
 class MishapNoTimeBomb : Mishap() {
     override fun accentColor(
@@ -15,11 +16,14 @@ class MishapNoTimeBomb : Mishap() {
     override fun errorMessage(
         ctx: CastingEnvironment,
         errorCtx: Context
-    ) = error("no_time_bomb")
+    ) = error("yaha:no_time_bomb")
 
     override fun execute(
         env: CastingEnvironment,
         errorCtx: Context,
         stack: MutableList<Iota>
-    ) {}
+    ) {
+        val pos = env.castingEntity?.pos ?: return
+        env.world.createExplosion(null, pos.x, pos.y, pos.z, 0.25f, World.ExplosionSourceType.NONE)
+    }
 }
